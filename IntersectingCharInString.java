1baba698088760bd1be728f6217098265b815fe2
Raja Imaya Bharathi <113758147+RajaImayaBharathi@users.noreply.github.com>
2024-06-11 22:03:41 +0530

    Create IntersectingCharInString.java
    import java.util.*;
    public class Main{
    public static void main(String[] args) {
    Scanner sc = new Scanner(System.in);
    String J = sc.nextLine();
    String S = sc.nextLine();
    Set<Character> jewelSet = new HashSet<>();
    for (char c : J.toCharArray()) {
    jewelSet.add(c);
    }
    int count = 0;
    for (char c : S.toCharArray()) {
    if (jewelSet.contains(c)) {
    count++;
    }
    }
    System.out.println(count);
    }
    }

---
+++ IntersectingCharInString.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main{
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+String J = sc.nextLine();
+String S = sc.nextLine();
+Set<Character> jewelSet = new HashSet<>();
+for (char c : J.toCharArray()) {
+jewelSet.add(c);
+}
+int count = 0;
+for (char c : S.toCharArray()) {
+if (jewelSet.contains(c)) {
+count++;
+}
+}
+System.out.println(count);
+}
+}
